0aeae6daa90c9a17264ef15f4534fd9a2443530e
Karen Cecena <karencecena@Karens-MacBook-Air.local>
2022-09-19 16:08:00 -0700

    Functions Added

--- game.py
+++ game.py
@@ -2,35 +2,51 @@
 
 import random
 
-# greet player
-print("hello!")
+def greeting():
+    # greet player
+    print("hello!")
 
-# get player name
-name = input("What's your name?: ")
-print(f"Hi {name}, nice to meet you!")
+    # get player name
+    name = input("What's your name?: ")
+    print(f"Hi {name}, nice to meet you!")
 
-# choose random number between 1 and 100
-number = random.randrange(1,101)
+    # choose random number between 1 and 100
 
-# repeat forever:
-while True:
-    guess = int(input("Choose a number between 1-100:"))
-    
+def guess_num():
+    number = random.randrange(1,101)
+    guess_count = 0
+    # repeat forever:
+    while True:
+        guess = input("Choose a number between 1-100:")
+        
+        if guess.isdigit():
+            guess = int(guess)
 
+            if guess in range(1,101): 
+        #     if guess is incorrect:
+                if guess > number:
+                    print("Too high!")
+                    guess_count += 1
+                elif guess < number:
+                    print("Too low!")
+                    guess_count += 1
+                else:
+                    print(f"Congrats! {guess} is the correct number. You had {guess_count} attempts.")
+            else:
+                print(f"{guess} is out of range, please choose a number between 1-100.")
 
-#### still needs to scold them if they do not put in an integer
-    if guess.isint():
-        guess = int(guess)
-        guess_count = 0
-    #     if guess is incorrect:
-        if guess > number:
-            print("Too high!")
-            guess_count += 1
-        elif guess < number:
-            print("Too low!")
-            guess_count += 1
         else:
-            print(f"Congrats! {guess} is the correct number")
-            
+            print(f"{guess} is out of range, please choose a number between 1-100.")
+
+def play_again()
+    answer = input("Do you want to play again? (Y or N): ")
+    if answer == "Y":
+        guess_num()
     else:
-        print("Sorry, I don't play with people who don't follow rules.")
+        print("ok bye")
+
+### Run Program:
+
+greeting()
+guess_num()
+play_again()
