c437fc5f5e5a1b7cbb7f98ae4c721982b9d7579d
Karen Cecena <karencecena@Karens-MacBook-Air.local>
2022-09-19 13:02:56 -0700

    Initial commit

---
+++ game.py
@@ -0,0 +1,15 @@
+"""A number-guessing game."""
+
+print("hi")
+
+# greet player
+# get player name
+# choose random number between 1 and 100
+# repeat forever:
+#     get guess
+#     if guess is incorrect:
+#         give hint
+#         increase number of guesses
+#     else:
+#         congratulate player
+
