5fbc3e2f679b911f0d7dc4e0a1a0d6b9a2480f25
Karen Cecena <karencecena@Karens-MacBook-Air.local>
2022-09-19 15:04:54 -0700

    Receive only numbers

--- game.py
+++ game.py
@@ -15,13 +15,22 @@ number = random.randrange(1,101)
 # repeat forever:
 while True:
     guess = int(input("Choose a number between 1-100:"))
-    guess_count = 0
-#     if guess is incorrect:
-    if guess > number:
-        print("Too high!")
-        guess_count += 1
-    elif guess < number:
-        print("Too low!")
-        guess_count += 1
+    
+
+
+#### still needs to scold them if they do not put in an integer
+    if guess.isint():
+        guess = int(guess)
+        guess_count = 0
+    #     if guess is incorrect:
+        if guess > number:
+            print("Too high!")
+            guess_count += 1
+        elif guess < number:
+            print("Too low!")
+            guess_count += 1
+        else:
+            print(f"Congrats! {guess} is the correct number")
+            
     else:
-        print(f"Congrats! {guess} is the correct number")
+        print("Sorry, I don't play with people who don't follow rules.")
