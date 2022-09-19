09eb3e2e0b5c8a07b08e4b82a5db12bf43a17878
Karen Cecena <karencecena@Karens-MacBook-Air.local>
2022-09-19 13:51:22 -0700

    create loop

--- game.py
+++ game.py
@@ -6,3 +6,20 @@ print("hello!")
 # get player name
 name = input("What's your name?: ")
 print(f"Hi {name}, nice to meet you!")
+
+# choose random number between 1 and 100
+number = 30
+
+# repeat forever:
+while True:
+    guess = int(input("Choose a number between 1-100:"))
+    guess_count = 0
+#     if guess is incorrect:
+    if guess > number:
+        print("Too high!")
+        guess_count += 1
+    elif guess < number:
+        print("Too low!")
+        guess_count += 1
+    else:
+        print(f"Congrats! {guess} is the correct number")
