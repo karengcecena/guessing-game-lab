5f4b0d9d228d8af69ab5203b4aa873318c811f91
Karen Cecena <karencecena@Karens-MacBook-Air.local>
2022-09-19 18:01:54 -0700

    tracks score, limits guesses, and user can add own range

--- game.py
+++ game.py
@@ -13,40 +13,53 @@ def greeting():
     # choose random number between 1 and 100
 
 def guess_num():
-    number = random.randrange(1,101)
-    guess_count = 0
-    # repeat forever:
-    while True:
-        guess = input("Choose a number between 1-100:")
-        
-        if guess.isdigit():
-            guess = int(guess)
+    a = int(input("Set bottom range: "))
+    b = int(input("Set upper range: "))
 
-            if guess in range(1,101): 
-        #     if guess is incorrect:
-                if guess > number:
-                    print("Too high!")
-                    guess_count += 1
-                elif guess < number:
-                    print("Too low!")
-                    guess_count += 1
+    number = random.randrange(a,b+1)
+    guess_count = 1
+    scores = []
+
+    while True:
+        if guess_count < 4:
+            guess = input(f"Choose a number between {a}-{b}:")
+            
+            if guess.isdigit():
+                guess = int(guess)
+
+                if guess in range(a,b+1): 
+            #     if guess is incorrect:
+                    if guess > number:
+                        print("Too high!")
+                        guess_count += 1
+                    elif guess < number:
+                        print("Too low!")
+                        guess_count += 1
+                    else:
+                        print(f"Congrats! {guess} is the correct number. You had {guess_count} attempts.")
+                        scores.append(guess_count)
+                        
+                        play_again()
+                        final_score = sorted(scores)
+                        print(f"Your best score was {final_score[0]}")
+                        
                 else:
-                    print(f"Congrats! {guess} is the correct number. You had {guess_count} attempts.")
+                    print(f"{guess} is out of range, please choose a number between {a}-{b}.")
+
             else:
-                print(f"{guess} is out of range, please choose a number between 1-100.")
+                print(f"{guess} is out of range, please choose a number between {a}-{b}.")
 
         else:
-            print(f"{guess} is out of range, please choose a number between 1-100.")
+            answer = input(f"Too many tries... Want to play again? ")
+            if answer == "Y" or answer == "y":
+                guess_num()
 
-def play_again()
+def play_again():
     answer = input("Do you want to play again? (Y or N): ")
-    if answer == "Y":
-        guess_num()
-    else:
-        print("ok bye")
+    if answer == "Y" or answer == "y":
+        guess_num()   
 
 ### Run Program:
 
 greeting()
 guess_num()
-play_again()
